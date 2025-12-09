69d868f2b68e54e66d50300bfbf7a02946979379
JamesBracken <james_bracken@yahoo.com>
2025-12-09 15:17:38 +0000

    Add: AWS Amplify & test

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@ import './App.css';
 import Nav from './components/navBar/nav';
 import ToDoList from './components/toDoList/ToDoList';
 import AddTask from './components/addTask.tsx/AddTask';
+import { logIn, loginTest } from './login';
+import config from "./config.json";
+
 function App() {
 
   return (
     <>
+      <button id='counter' onClick={() => {
+        loginTest()
+      }
+      }>Click me</button>
       <Nav />
       <AddTask />
       <ToDoList />
--- src/components/navBar/Nav.tsx
+++ src/components/navBar/Nav.tsx
@@ -2,7 +2,7 @@ const Nav = () => {
     return (
         <>
             <nav>
-                <a href="">Login</a>
+                <a href="https://eu-north-1dsoci5dtk.auth.eu-north-1.amazoncognito.com/login?client_id=o817ick1fj45frs5gg42nv0sq&redirect_uri=https://d84l1y8p4kdic.cloudfront.net&response_type=code&scope=email+openid+phone">Login</a>
                 <a href="">Signup</a>
             </nav>
         </>
